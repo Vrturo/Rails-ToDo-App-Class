5d559e2d36f3a90a5bd92dc4aa5501553cfa331d
name": null, & Arturo Perez <pair+dknittel.vrturo@devbootcamp.com>
2015-09-10 11:31:09 -0700

    fill tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,24 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    #render form
+  end
 
+  def new
 
+  end
+
+  def create
+    @task = Task.create(task_params)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:description, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/new'
+
+  get 'tasks/create'
+
+  get 'tasks/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
